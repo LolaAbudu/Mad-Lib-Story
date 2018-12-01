80d947e38500674ea1c67086fd353883796cf054
LolaAbudu <lolaabudu1589@gmail.com>
2018-12-01 18:24:20 -0500

    fixed email link, refactored activity 8

--- app/src/main/java/com/example/lolaabudu/story_app_hw_abudu_omolola/EighthActivity.java
+++ app/src/main/java/com/example/lolaabudu/story_app_hw_abudu_omolola/EighthActivity.java
@@ -3,20 +3,27 @@ package com.example.lolaabudu.story_app_hw_abudu_omolola;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.Random;
 
 public class EighthActivity extends AppCompatActivity {
 
     private EditText emailEditText8;
+    private Button button;
+    private ConstraintLayout layout;
+    private TextView textView;
     private String[] mColorArray = {"red", "pink", "purple", "deep_purple",
             "indigo", "blue", "light_blue", "cyan", "teal", "green",
             "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
@@ -27,55 +34,59 @@ public class EighthActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eighth);
-
         Random random = new Random();
         String backgroundColor = mColorArray[random.nextInt(20)];
         int colorResourceName = getResources().getIdentifier(backgroundColor,
                 "color", getApplicationContext().getPackageName());
+        textView = findViewById(R.id.story_textView);
+        emailEditText8 = findViewById(R.id.editText8);
+        button = findViewById(R.id.button8);
+        layout = findViewById(R.id.eighth_activity);
         int colorRes = ContextCompat.getColor(this, colorResourceName);
-        ConstraintLayout layout = findViewById(R.id.eighth_activity);
         layout.setBackgroundColor(colorRes);
 
-        Intent intent = getIntent();
-        final String message1 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE1);
-        final String message2 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE2);
-        final String message3 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE3);
-        final String message4 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE4);
-        final String message5 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE5);
-        final String message6 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE6);
-        final String message7 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE7);
-        final String concatenatedMessage = "So I wanted to tell a story about something but had no idea what to write about, so I decided to just make 'something\' up. Everyday I wake up thinking why is it " +
-                message1 + " already, then I realize that I am a whole day older today. I lay there and think about my " +
-                message2 + " and what the hell I am doing here. So I close my eyes and go right back to sleep. Dream time! Yaaaay! My favorite time of day. Jumping over " +
-                message3 + " and flying through " +
-                message4 + ", I realize how amazing dream-land can really be. Finding myself typing on a " +
-                message5 + ", click click click is all I hear. As he drinks his bottle and hums a rhythm. Hurry up and eat little boy, so I can get this " +
-                message6 + " done. Okay. Stop! Its play time. Time for mental release and " +
-                message7 + " talk. I love these moments of time. Am I awake or still asleep? Not quite sure but time keeps flying and so do I.";
+        final String concatenatedMessage = getStrings();
 
-        TextView textView = findViewById(R.id.story_textView);
-        textView.setText(concatenatedMessage);
-
-
-        Button button = findViewById(R.id.button8);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                emailEditText8 = findViewById(R.id.editText8);
+                String emailAddress = emailEditText8.getText().toString();
 
-                if (emailEditText8.getText().toString().equals("")) {
+                if (TextUtils.isEmpty(emailAddress)) {
                     Toast.makeText(v.getContext(), "Enter Email Address", Toast.LENGTH_SHORT).show();
                 } else {
-                    String emailAddress = emailEditText8.getText().toString();
-
-                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                    Uri uri = Uri.fromParts(
+                            "mailto", emailAddress, null);
+                    Intent emailIntent = new Intent(Intent.ACTION_SENDTO, uri);
                     emailIntent.putExtra(Intent.EXTRA_EMAIL, emailAddress);
                     emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Mad-Lib! What a Story!");
                     emailIntent.putExtra(Intent.EXTRA_TEXT, concatenatedMessage);
-                    emailIntent.setType("message/text");
                     startActivity(Intent.createChooser(emailIntent, "Choose an Email client :"));
                 }
             }
         });
+    }
+
+    @NonNull
+    private String getStrings() {
+        Intent intent = getIntent();
+        String message1 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE1);
+        String message2 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE2);
+        String message3 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE3);
+        String message4 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE4);
+        String message5 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE5);
+        String message6 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE6);
+        String message7 = intent.getStringExtra(SeventhActivity.EXTRA_MESSAGE7);
+        String concatenatedMessage = getString(R.string.concatenated_message,
+                message1,
+                message2,
+                message3,
+                message4,
+                message5,
+                message6,
+                message7);
+
+        textView.setText(concatenatedMessage);
+        return concatenatedMessage;
     }
 }
